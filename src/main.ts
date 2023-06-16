9ffa886638e15aef375a45a37a430a0e648483c8
Maciej Preis <maciej.preis@onet.eu>
2023-06-16 12:50:00 +0200

    feat: config

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import './tailwind.css'
 import App from './App.vue'
 import router from './router'
 import i18n from './plugins/i18n'
+import { createPinia } from 'pinia'
 
 createApp(App)
+  .use(createPinia())
   .use(router)
   .use(i18n)
   .mount('#app')
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,5 +2,22 @@ module.exports = {
   content: [
     './index.html',
     './src/**/*.{ts,vue}'
-  ]
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'custom-green': {
+          500: '#459671'
+        },
+        'custom-gray': {
+          100: '#F8F9FA',
+          200: '#F6F7F9',
+          300: '#AEB5BC',
+          400: '#6D747C',
+          500: '#4A4F56',
+          700: '#4C535A'
+        }
+      }
+    }
+  }
 }
