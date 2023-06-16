2037c4caf5b2c5d24315dfb6c5185d80d9ed309c
Maciej Preis <maciej.preis@onet.eu>
2023-06-16 12:50:36 +0200

    feat: molecules

---
+++ src/components/molecules/ListHeader/ListHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="md:flex md:justify-between">
+    <div class="md:bg-custom-gray-100 md:w-96 rounded flex justify-between">
+      <input
+        class="bg-custom-gray-100 px-2 py-1 placeholder:text-custom-gray-400 focus:outline-none focus:ring-0 w-full md:w-auto"
+        :placeholder="$t('list.placeholder')"
+      >
+      <icon
+        icon="search"
+        viewBox="0 0 50 50"
+        class="w-4 h-4 mt-3 mx-4 hidden md:block text-custom-gray-400 stroke-custom-gray-400 stroke-[4]"
+      />
+    </div>
+    <button
+      class="flex bg-custom-green-500 rounded-full px-6 py-2 text-white font-semibold w-full md:w-auto mt-4 md:mt-0 justify-center md:justify-normal whitespace-nowrap"
+    >
+      <icon
+        icon="plus"
+        viewBox="0 0 24 24"
+        class="w-4 h-4 mr-2 mt-[3px] stroke-white stroke-2"
+      />
+      {{ $t('list.addUser') }}
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Icon from '@atoms/Icon'
+
+export default defineComponent({
+  name: 'MoleculesListHeader',
+  components: {
+    Icon
+  }
+})
+</script>
---
+++ src/components/molecules/ListRow/ListRow.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="flex justify-between py-6 rounded-md even:bg-custom-gray-200">
+    <div class="flex space-x-4 md:space-x-16 pl-2 md:pl-4">
+      <avatar :path="row.avatar" />
+      <span
+        class="text-custom-gray-700 my-auto"
+        v-text="`${row.first_name} ${row.last_name}`"
+      />
+    </div>
+    <div
+      class="mr-2 md:mr-12 flex space-x-2 my-auto"
+    >
+      <icon
+        viewBox="0 0 32 32"
+        icon="edit"
+        class="h-6 w-6 text-custom-gray-300 cursor-pointer hover:text-gray-500"
+        @click="$emit('edit', row.id)"
+      />
+      <icon
+        viewBox="0 0 32 32"
+        icon="trash"
+        class="h-6 w-6 pt-0.5 text-custom-gray-300 cursor-pointer hover:text-gray-500"
+        @click="$emit('delete', row)"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Avatar from '@atoms/Avatar'
+import Icon from '@atoms/Icon'
+
+export default defineComponent({
+  name: 'MoleculesListHeader',
+  components: {
+    Avatar,
+    Icon
+  },
+  props: {
+    row: {
+      type: Object,
+      required: true
+    }
+  },
+  emits: ['edit', 'delete']
+})
+</script>
