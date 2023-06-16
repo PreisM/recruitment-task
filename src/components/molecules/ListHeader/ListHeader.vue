0ee6d33868f34abadadede82cc90ce20b41c9f9d
Maciej Preis <maciej.preis@onet.eu>
2023-06-16 20:21:02 +0200

    fix: listHeader

--- src/components/molecules/ListHeader/ListHeader.vue
+++ src/components/molecules/ListHeader/ListHeader.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="md:flex md:justify-between">
-    <div class="md:bg-custom-gray-100 md:w-96 rounded flex justify-between">
+    <div class="md:w-96 rounded flex justify-between relative">
       <input
-        class="bg-custom-gray-100 px-2 py-1 placeholder:text-custom-gray-400 focus:outline-none focus:ring-0 w-full md:w-auto"
+        class="bg-custom-gray-100 px-2 py-1 placeholder:text-custom-gray-400 focus:outline-none focus:ring-0 w-full"
         :placeholder="$t('list.placeholder')"
+        onkeydown="return /[a-z]/i.test(event.key)"
+        @input="setSearchPhrase"
       >
       <icon
         icon="search"
         viewBox="0 0 50 50"
-        class="w-4 h-4 mt-3 mx-4 hidden md:block text-custom-gray-400 stroke-custom-gray-400 stroke-[4]"
+        class="w-4 h-4 mt-3 mx-4 absolute -translate-y-1 md:translate-y-0 top-0 right-0 text-custom-gray-400 stroke-custom-gray-400 stroke-[4]"
       />
     </div>
     <button
       class="flex bg-custom-green-500 rounded-full px-6 py-2 text-white font-semibold w-full md:w-auto mt-4 md:mt-0 justify-center md:justify-normal whitespace-nowrap"
+      @click="$router.push('/add')"
     >
       <icon
         icon="plus"
@@ -25,13 +28,25 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import Icon from '@atoms/Icon'
 
 export default defineComponent({
   name: 'MoleculesListHeader',
   components: {
     Icon
+  },
+  emits: ['search'],
+  setup (_, { emit }) {
+    const phrase = ref('')
+
+    const setSearchPhrase = (ev: Event) => {
+      emit('search', (ev.target as HTMLInputElement).value)
+    }
+    return {
+      phrase,
+      setSearchPhrase
+    }
   }
 })
 </script>
